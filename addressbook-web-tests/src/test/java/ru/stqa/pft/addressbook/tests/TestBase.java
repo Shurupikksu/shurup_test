304f42454bea9f6ebde63d11a12c59be9a49680a
Ksu Kuzzi <blairwitch@mail.ru>
2019-02-12 21:18:29 +0300

    комментарии

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,17 +6,17 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase { // теперь этот класс перед каждым тестом инициализирует обьект app типа ApplicationManager, запускает и в конце останавливает
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(); // ссылка на applicationManager
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
-        app.init(); // тут вызываются методы в обьекте типа ApplicationManager
+        app.init(); // тут вызываются методы в обьекте типа ApplicationManager для подключения драйвера и логина
 
     }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
-        app.stop();
+        app.stop(); // для выхода
     }
 
 }
